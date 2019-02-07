8f362d2d46fc28f3b4ec4a5dbf6c40c67afe8d2f
Sergey <serg.gavrilchenko@gmail.com>
2019-02-07 23:28:09 +0200

    Update to react hooks

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
-import React, { Component, StrictMode } from 'react';
+import React, { StrictMode } from 'react';
 import { hot } from 'react-hot-loader/root';
 import { Header } from 'Components';
 // import { app } from "./App.scss";
 
-class App extends Component {
-  render() {
-    return (
-      <StrictMode>
-        <Header name="Sergey" />
-      </StrictMode>
-    );
-  }
-}
+const App = () => {
+  return (
+    <StrictMode>
+      <Header name="Sergey" />
+    </StrictMode>
+  );
+};
 
 export default (process.env.NODE_ENV === 'development' ? hot(App) : App);
