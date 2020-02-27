d0160a59d7985c9a3ee430606f67828a70c871b0
sergey <serg.gavrilchenko@gmail.com>
2020-02-27 23:49:49 +0200

    update packages and delete hot-loader

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,8 @@ const { resolve } = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const webpack = require('webpack');
 
 module.exports = (env) => {
-  let hotPlugin;
   let styleLoader;
   let cssExtractPlugin;
   let isSourceMap;
@@ -22,7 +20,6 @@ module.exports = (env) => {
     default:
       styleLoader = { loader: 'style-loader' };
       isSourceMap = 'inline-source-map';
-      hotPlugin = new webpack.HotModuleReplacementPlugin();
   }
 
   const config = {
@@ -95,7 +92,6 @@ module.exports = (env) => {
       open: true,
       port: 6289,
       historyApiFallback: true,
-      hot: true,
       progress: true,
     },
 
@@ -114,15 +110,12 @@ module.exports = (env) => {
         Containers: resolve(__dirname, 'src/containers/'),
         Actions: resolve(__dirname, 'src/actions/'),
         images: resolve(__dirname, 'src/images/'),
-        'react-dom': '@hot-loader/react-dom',
       },
     },
   };
 
   if (env.mode === 'production') {
     config.plugins.push(cssExtractPlugin);
-  } else {
-    config.plugins.push(hotPlugin);
   }
 
   return config;
